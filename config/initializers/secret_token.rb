79f4233323d5fff5f3a8b6506a6a721f3fc2f23b
inoe <inoe@inoe-Inspiron-1420.(none)>
2011-09-08 11:04:29 -0400

    skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inno::Application.config.secret_token = '039f525e4891c829c9d5989e77cf3963ff552b7d37a84192a658936e678f9430a9636d4a3ded06e117224013ca5c5929aeb12275878430305ec22bb6d14e93de'
